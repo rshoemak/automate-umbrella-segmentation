81c88ded12c3c29edc37b1038827bb9f6880257c
Ryan Shoemaker <rshoemak@cisco.com>
2022-08-24 11:25:42 -0500

    Included multiple VPNs

--- umb-seg-v2.py
+++ umb-seg-v2.py
@@ -16,8 +16,11 @@ ROUTER = '1.3.9.1'
 UMBORGID = '6358063'
 UMBMGMTKEY = 'f2aeb2a241714916a798d0f1a296400c'
 UMBMGMTSEC = 'df721d3593454f3294aab626dc7ef759'
+#VPNID = '10'
 UMBDEVKEY = '3b8a0835ae4e44428f41b654efde198b'
 UMBDEVSEC = '4e8094dca7f446f598db169f8fa33b0f'
+#VPN = 'Corporate'
+#WEBPOLICY = 'Corporate-Segment-Web-Policy'
 
 VPNLIST = [{'name':'Corporate','vpn':'10','webpolicy':'Corporate-Segment-Web-Policy'},
            {'name':'Guest','vpn':'20','webpolicy':'Guest-Segment-Web-Policy'},
